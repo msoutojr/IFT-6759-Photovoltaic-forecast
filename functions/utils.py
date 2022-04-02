94ec43ec751e52935cc8c04d192358ccf4f35ecd
msoutojr <msouto_jr@yahoo.com.br>
2022-04-02 18:33:51 -0400

    sunset_time

--- functions/utils.py
+++ functions/utils.py
@@ -15,15 +15,17 @@ def train(model, train_loader, optimizer, loss_fn, device):
     model.train()
     for batch_idx, (inputs, target) in enumerate(train_loader):  
         
-        #inputs = inputs.to(device)
-        #target = target.to(device)
         optimizer.zero_grad()
         
-        output = model.forward(inputs.to(device).float())
-        loss = loss_fn(output.float().view(output.shape[0]), target.to(device).float())
+        inputs = inputs.to(device).float()
+        target = target.to(device).float().view(target.shape[0],1)
+        
+        output = model.forward(inputs)
+        
+        loss = torch.sqrt(loss_fn(output, target))
         
         with torch.no_grad():
-            loss_train += loss.item()   
+            loss_train += (loss.item()**2)*inputs.shape[0]   
         
         loss.backward()
         optimizer.step()
@@ -44,13 +46,104 @@ def test(model, test_loader, optimizer, loss_fn, device):
     
         for batch_idx, (inputs, target) in enumerate(test_loader):
             
-            #inputs = inputs.to(device)
-            #target = target.to(device)
+            inputs = inputs.to(device).float()
+            target = target.to(device).float().view(target.shape[0],1)
             
-            output = model.forward(inputs.to(device).float())
-            loss = loss_fn(output.float().view(output.shape[0]), target.to(device).float())
-            loss_test += loss.item()
+            output = model.forward(inputs)
             
-            #print('batch: '+str(batch_idx) + ' , inputs shape:' + str(inputs.shape) + ' , loss = ' + str(loss.item()))
-    
+            loss = torch.sqrt(loss_fn(output, target))
+            loss_test += (loss.item()**2)*inputs.shape[0]
+            
     return loss_test
+    
+def test2(model, test_loader, optimizer, loss_fn, device):
+    
+    """Evaluate the model by doing one pass over a test dataset"""
+    
+    # test
+    
+    loss_test = 0
+    
+    model.eval()
+    with torch.no_grad():
+    
+        for batch_idx, (inputs, target) in enumerate(test_loader):
+            
+            inputs = inputs.to(device).float()
+            target = target.to(device).float().view(target.shape[0],1)
+            
+            output = model.forward(inputs)
+            
+            loss = torch.sqrt(loss_fn(output, target))
+            loss_test += (loss.item())
+            
+    return (loss_test)/(len(test_loader))
+    
+
+def train_gru(model, train_loader, optimizer, loss_fn, device, seq_len):
+    
+    """Perform one epoch of training the model by doing one pass over a train dataset"""
+
+    loss_train = 0
+    n=0
+    model.train()
+    
+    for batch_idx, (inputs, target) in enumerate(train_loader):  
+        
+        optimizer.zero_grad()
+        
+        target = target.view(target.shape[0],1).float().to(device)
+        
+        if seq_len > 1:
+            inputs = inputs.view(inputs.shape[0],seq_len,3,64,64).float().to(device)
+        else:
+            inputs = inputs.float().to(device)
+            
+
+        output = model(inputs).cuda()
+
+        loss = loss_fn(output, target)
+        
+        with torch.no_grad():
+            n_i = inputs.shape[0]
+            loss_train += loss.item()*n_i 
+            n += n_i        
+        
+        loss.backward()
+        optimizer.step()
+        
+        
+    return loss_train/n
+ 
+    
+def test_gru(model, test_loader, optimizer, loss_fn, device, seq_len):
+    
+    """Evaluate the model by doing one pass over a test dataset"""
+    
+
+    loss_test = 0
+    
+    n=0
+    
+    model.eval()
+    with torch.no_grad():
+    
+        for batch_idx, (inputs, target) in enumerate(test_loader):
+            
+            
+            target = target.view(target.shape[0],1).float().to(device)
+        
+            if seq_len > 1:
+                inputs = inputs.view(inputs.shape[0],seq_len,3,64,64).float().to(device)
+            else:
+                inputs = inputs.float().to(device)
+            
+            output = model(inputs)
+            loss = torch.sqrt(loss_fn(output, target))
+            
+            n_i = inputs.shape[0]
+            loss_test += (loss.item()**2)*n_i 
+            n += n_i        
+
+            
+    return loss_test/n
--- models/SUNSET.py
+++ models/SUNSET.py
@@ -43,7 +43,7 @@ class SUNSETBase(nn.Module):
         Compute forward pass for the model.
         nn.Module will automatically create the `.backward` method!
         """
-        
+        x = x.cuda().float()
         x = self.conv1(x)
         x = F.relu(x)
         x = self.bn1(x)
@@ -67,7 +67,7 @@ class SUNSETBase(nn.Module):
         x = self.reg(x)
         
         return x
-
+    
         
 class SUNSET_Sunny(nn.Module):
     
@@ -243,3 +243,140 @@ class SUNSET_Overcast(nn.Module):
         x = self.reg(x)
         
         return x
+
+
+
+class SUNSET_GRU4(nn.Module):
+    
+    """
+
+    """
+    def __init__(self, conv_layers=2, convGRU_layers = 3, fc_layers = 2 ):
+        
+        super(SUNSET_GRU4, self).__init__()
+        
+        import models.convGRU as convGRU
+
+        if conv_layers > 2:
+            conv_layers = 2
+            print('max conv layers = 2')
+        self.conv_layers = conv_layers
+        
+        if convGRU_layers > 3:
+            convGRU_layers = 3
+            print('max number of convGRU layers = 3')
+        self.convGRU_layers = convGRU_layers
+        
+        if fc_layers > 2:
+            fc_layers == 2
+            print('max fc layers = 2')
+        self.fc_layers = fc_layers
+    
+        if conv_layers == 2:
+            chanels = [3,24,48]
+        elif conv_layers == 1:
+            chanels = [3,24]
+        else:
+            chanels = [3]
+        
+        #Conv Layers            
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=24, kernel_size = 3, padding="same")
+        self.conv2 = nn.Conv2d(in_channels=24, out_channels=48, kernel_size = 3, padding="same")
+        self.bn1 = nn.BatchNorm2d(24)
+        self.bn2 = nn.BatchNorm2d(48)
+        self.maxpool1 = nn.MaxPool2d(2)
+        self.maxpool2 = nn.MaxPool2d(2)
+        
+        # ConvGRU        
+        input_size = ( int(64/(2**conv_layers)), int(64/(2**conv_layers)) )
+        input_dim = int(chanels[-1])
+        hidden_dim_set = [32,32,32]
+        kernel_size_set = [(3,3),(3,3),(3,3)]
+        hidden_dim = []
+        kernel_size = []
+        for i in range(convGRU_layers):
+            hidden_dim.append(int(hidden_dim_set[i]))
+            kernel_size.append(kernel_size_set[i])
+        num_layers = convGRU_layers
+        dtype = torch.cuda.FloatTensor
+        batch_first=True
+        bias = False
+        return_all_layers=False
+        self.GRU = convGRU.ConvGRU(input_size,input_dim,hidden_dim,kernel_size,num_layers,dtype,batch_first,bias,return_all_layers).cuda()
+        
+        
+        # FC layers
+        if convGRU_layers > 0:
+            dim_0 = int(hidden_dim[-1]*input_size[0]*input_size[1])
+        else:
+            dim_0 = int(chanels[-1]*input_size[0]*input_size[1])
+        if fc_layers == 0:
+            dim_reg = dim_0
+        else:
+            dim_reg = 1024
+        self.drop_rate = 0.4
+        self.fc1 = nn.Linear(dim_0, 1024)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.drop1 = nn.Dropout(self.drop_rate)
+        self.drop2 = nn.Dropout(self.drop_rate)
+        self.reg = nn.Linear(dim_reg, 1)
+
+    
+    
+    def forward(self, inputs=None, device="cuda:0", hidden_state=None):
+        
+        batch = inputs.shape[0]
+        if inputs.dim() == 5:
+            seq_len = inputs.shape[1]
+        else:
+            seq_len = 1
+        
+        C = inputs.shape[-3]
+        H = inputs.shape[-2]
+        W = inputs.shape[-1]
+        
+        if self.conv_layers > 0:
+            x = inputs.view(batch*seq_len,C,H,W)
+        else:
+            x = inputs
+        
+        if self.conv_layers > 0:
+                x = self.conv1(x)
+                x = F.relu(x)
+                x = self.bn1(x)
+                x = self.maxpool1(x)
+        
+        if self.conv_layers == 2:
+                x = self.conv2(x)
+                x = F.relu(x)
+                x = self.bn2(x)
+                x = self.maxpool2(x)
+        
+        
+        C = x.shape[-3]
+        H = x.shape[-2]
+        W = x.shape[-1]
+        
+        if self.convGRU_layers > 0:
+                x = x.view(batch,seq_len,C,H,W)
+                x,h = self.GRU(x)
+                x = torch.flatten(x[0][:,-1,:,:,:], start_dim=1).to(device)
+        else:
+                x = torch.flatten(x, start_dim=1).to(device)
+        
+        
+        if self.fc_layers > 0:
+                x = self.fc1(x)        
+                x = F.relu(x)
+                x = self.drop1(x)
+        
+        if self.fc_layers == 2:
+                x = self.fc2(x)
+                x = F.relu(x)
+                x = self.drop2(x)
+        
+        output = self.reg(x)
+        return output
+        
+        
+        
