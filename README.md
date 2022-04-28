82306a7648b7a49c22ef623e1c19c3a8eddab972
msoutojr <msouto_jr@yahoo.com.br>
2022-04-28 15:19:30 -0400

    update4

---
+++ README.md
@@ -0,0 +1,39 @@
+# IFT 3710/6759 - Projets (avancés) en apprentissage automatique
+## Project: Photovoltaic Power and Solar Radiation Forecasting
+
+Project developed during the course IFT-6759 at the University of Montreal, in the winter semester 2022.
+
+## Team:
+* Marcos A. A. Souto Jr.
+* Kodjovi Adabra
+* Niranjan Niranjan
+* Rui Ze Ma
+
+## Project Goal
+
+The main theme of the project is photovoltaic energy generation forecast. The objective is to reproduce and extend the methodology to improve the results found in the reference paper [1], , that uses a deep learning approach to predict power output from images of the sky captured with a camera installed near the solar panels.
+
+## Instructions
+
+Notebooks in the main folder are independent.
+
+Notebooks 1 and 2 are part of the reference paper replication step.
+
+Notebooks 3 to 5 are the experiences developed by the group seeking to improve the performance of the model. These notebooks can be run independently.
+
+Some notebooks have a lightweight version for the purpose of verifying the functioning of the algorithms.
+
+The experiments were executed using python 3.7.
+
+---
+
+## Third part modules
+
+* Convolutional Gated Recurrent Unit (ConvGRU) in PyTorch (Copyright 2017 Jacob C. Kimmel): https://github.com/jacobkimmel/pytorch_convgru 
+
+## References
+
+[1] Yuhao Nie, Yuchi Sun,  Yuanlei Chen, Rachel Orsini, and  Adam Brandt. PV power output prediction from sky images using convolutional neural network: The comparison of sky-condition-specific submodels and an end-to-end model. J. Renewable Sustainable Energy 12, 046101 (2020); https://doi.org/10.1063/5.0014016.
+
+
+
--- functions/utils.py
+++ functions/utils.py
@@ -78,7 +78,75 @@ def test2(model, test_loader, optimizer, loss_fn, device):
             loss_test += (loss.item())
             
     return (loss_test)/(len(test_loader))
+  
+ 
+def train_sunset(model, train_loader, optimizer, loss_fn, device, seq_len):
     
+    """Perform one epoch of training the model by doing one pass over a train dataset"""
+
+    loss_train = 0
+    n=0
+    model.train()
+    
+    for batch_idx, (inputs, target) in enumerate(train_loader):  
+        
+        optimizer.zero_grad()
+        
+        target = target.view(target.shape[0],1).float().to(device)
+        
+        if seq_len > 1:
+            inputs = inputs.view(inputs.shape[0],seq_len,3,64,64).float().to(device)
+        else:
+            inputs = inputs.float().to(device)
+            
+
+        output = model(inputs).cuda()
+
+        loss = loss_fn(output, target)
+        
+        with torch.no_grad():
+            n_i = inputs.shape[0]
+            loss_train += loss.item()*n_i 
+            n += n_i        
+        
+        loss.backward()
+        optimizer.step()
+        
+        
+    return loss_train/n
+ 
+    
+def test_sunset(model, test_loader, optimizer, loss_fn, device, seq_len):
+    
+    """Evaluate the model by doing one pass over a test dataset"""
+    
+
+    loss_test = 0
+    
+    n=0
+    
+    model.eval()
+    with torch.no_grad():
+    
+        for batch_idx, (inputs, target) in enumerate(test_loader):
+            
+            
+            target = target.view(target.shape[0],1).float().to(device)
+        
+            if seq_len > 1:
+                inputs = inputs.view(inputs.shape[0],seq_len,3,64,64).float().to(device)
+            else:
+                inputs = inputs.float().to(device)
+            
+            output = model(inputs)
+            loss = torch.sqrt(loss_fn(output, target))
+            
+            n_i = inputs.shape[0]
+            loss_test += (loss.item()**2)*n_i 
+            n += n_i        
+
+            
+    return loss_test/n  
 
 def train_gru(model, train_loader, optimizer, loss_fn, device, seq_len):
     
